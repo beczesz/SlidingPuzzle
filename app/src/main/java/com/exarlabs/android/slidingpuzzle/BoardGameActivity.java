c1f27f59b9767dc54d18f63cef035e348db09c2e
beczesz <beczesz@yahoo.com>
2015-09-14 17:57:25 +0300

    New lib: ButterKnife
     - Adding dependency on ButterKnife

--- app/src/main/java/com/exarlabs/android/slidingpuzzle/BoardGameActivity.java
+++ app/src/main/java/com/exarlabs/android/slidingpuzzle/BoardGameActivity.java
@@ -4,8 +4,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.exarlabs.android.slidingpuzzle.fragments.BoardFragment;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Activity which holds the board and connects with the model and the solver.
@@ -28,7 +34,11 @@ public class BoardGameActivity extends AppCompatActivity {
     // FIELDS
     // ------------------------------------------------------------------------
 
+    // The board fragment
     private BoardFragment mBoardFragment;
+
+    @Bind(R.id.shuffle_button)
+    public Button mShuffleButton;
 
     // ------------------------------------------------------------------------
     // CONSTRUCTORS
@@ -46,9 +56,10 @@ public class BoardGameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board_game);
+        ButterKnife.bind(this);
 
         // Embed the board
-        mBoardFragment =  new BoardFragment();
+        mBoardFragment = new BoardFragment();
         getSupportFragmentManager().beginTransaction().add(R.id.board_container, mBoardFragment).commit();
     }
 
@@ -56,9 +67,12 @@ public class BoardGameActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_board_game, menu);
-
-
         return true;
+    }
+
+    @OnClick(R.id.shuffle_button)
+    public void shuffleTiles() {
+        Toast.makeText(this, "Clicked!", Toast.LENGTH_SHORT).show();
     }
 
     @Override
